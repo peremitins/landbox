ef20ba4be8e9be49dfb9dc0b836326558afec26c
nikolay_peremitin <peremitinns@mail.ru>
2021-04-09 15:42:12 +0300

    add project

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+const body = document.body;
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+
+menuBtn.addEventListener('click', function () {
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+	body.classList.toggle('active');
+});
+
+//swiper 
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 22,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // centeredSlides: true,
+  breakpoints: {
+    510: {
+      slidesPerView: 2,
+    },
+    762: {
+      slidesPerView: 4,
+    }
+  }
+});
+//# sourceMappingURL=main.js.map
